5d040a366a5277df6d4071733dbda368c76a2151
Orderr147 <131309176+Orderr147@users.noreply.github.com>
2023-05-21 05:19:40 +0300

    Update product.js

--- product.js
+++ product.js
@@ -20,13 +20,13 @@ pro_container.innerHTML=product.repeat(8);
 //set card images
 var pro=document.getElementsByClassName("pro");
 // console.log(pro[0].childNodes[1])
-pro[0].childNodes[1].setAttribute("src","images/products/p1.png")
-pro[1].childNodes[1].setAttribute("src","images/products/p2.png")
-pro[2].childNodes[1].setAttribute("src","images/products/p3.png")
-pro[3].childNodes[1].setAttribute("src","images/products/p4.png")
-pro[4].childNodes[1].setAttribute("src","images/products/p5.PNG")
-pro[5].childNodes[1].setAttribute("src","images/products/p6.PNG")
-pro[6].childNodes[1].setAttribute("src","images/products/p7.png")
-pro[7].childNodes[1].setAttribute("src","images/products/p8.png")
+pro[0].childNodes[1].setAttribute("src","./Rgb.jpg")
+pro[1].childNodes[1].setAttribute("src","./Ctctvv.jpg")
+pro[2].childNodes[1].setAttribute("src","./R.jpg")
+pro[3].childNodes[1].setAttribute("src","./Dcf.jpg")
+pro[4].childNodes[1].setAttribute("src","./Nn.jpg")
+pro[5].childNodes[1].setAttribute("src","./Rr.jpg")
+pro[6].childNodes[1].setAttribute("src","./Hu.jpg")
+pro[7].childNodes[1].setAttribute("src","./Dffv.jpg")
 
 // var pro=document.getElementsByClassName("pro")
